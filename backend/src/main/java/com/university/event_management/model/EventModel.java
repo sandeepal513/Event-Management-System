e58dfa79d3798746acd339789f474dda1983fa61
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-03-29 20:40:50 +0530

    create event model

--- backend/src/main/java/com/university/event_management/model/EventModel.java
+++ backend/src/main/java/com/university/event_management/model/EventModel.java
@@ -1,4 +1,26 @@
 package com.university.event_management.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "events")
 public class EventModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String description;
+    private LocalDateTime date;
+    private String venue;
+
+
 }
