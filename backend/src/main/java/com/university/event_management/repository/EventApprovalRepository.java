a3f98ecbf10b209446a981a14eca297bdaccc355
Kumudu Amarakoon <kumudu4746@gmail.com>
2026-04-07 02:47:43 +0530

    create  EventApprovalRepository class and build

---
+++ backend/src/main/java/com/university/event_management/repository/EventApprovalRepository.java
@@ -0,0 +1,14 @@
+package com.university.event_management.repository;
+
+import com.university.event_management.model.EventApproval;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EventApprovalRepository extends JpaRepository<EventApproval, Integer> {
+
+    List<EventApproval> findByStatus(String status);
+
+    List<EventApproval> findByEventId(Integer eventId);
+
+}
