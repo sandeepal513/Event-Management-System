09638199adaf93803774ee41880819e302c95130
Kumudu Amarakoon <kumudu4746@gmail.com>
2026-04-08 01:21:14 +0530

    update RegistrationController java class

--- backend/src/main/java/com/university/event_management/controller/RegistrationController.java
+++ backend/src/main/java/com/university/event_management/controller/RegistrationController.java
@@ -7,27 +7,62 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
-@RequestMapping("/api/regitration")
+@RequestMapping("/api/registration")
 public class RegistrationController {
 
     @Autowired
     private RegistrationService registrationService;
 
+    //---------student-------------
+
+    //Student register for event
+    @PostMapping
+    public ResponseEntity<Registration> register(@RequestBody Map<String,Integer> body) {
+        return ResponseEntity.ok(registrationService.register(
+                body.get("userId"),
+                body.get("eventId")
+        ));
+    }
+
+    //Student sees their registrations
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Registration>> getByUser(@PathVariable Integer userId) {
+        return ResponseEntity.ok(registrationService.getByUser(userId));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> cancel(@PathVariable Integer id) {
+        registrationService.cancel(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    //------------Admin---------------
+
+    //Admin sees all registrations
     @GetMapping
     public ResponseEntity<List<Registration>> getAll() {
         return ResponseEntity.ok(registrationService.getAll());
     }
 
+    //Admin sees registration by event
+    @GetMapping("/event/{eventId}")
+    public ResponseEntity<List<Registration>> getByEventId(@PathVariable Integer eventId) {
+        return ResponseEntity.ok(registrationService.getByEventId(eventId));
+    }
+
+    //admin approves
     @PostMapping("/{id}/approve")
-    public ResponseEntity<Registration> approve(@PathVariable int id) {
+    public ResponseEntity<Registration> approve(@PathVariable Integer id) {
         return ResponseEntity.ok(registrationService.approve(id));
     }
 
+    //Admin rejects
     @PostMapping("/{id}/reject")
-    public ResponseEntity<Registration> reject(@PathVariable int id) {
+    public ResponseEntity<Registration> reject(@PathVariable Integer id) {
         return ResponseEntity.ok(registrationService.reject(id));
     }
 
