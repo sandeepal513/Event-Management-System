0bab85b7a3801e092de5fb3b987e2a7a403ca6b7
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-04-26 16:45:53 +0530

    feat: add transactional delete method for event approvals by event ID

--- backend/src/main/java/com/university/event_management/repository/EventApprovalRepository.java
+++ backend/src/main/java/com/university/event_management/repository/EventApprovalRepository.java
@@ -1,6 +1,7 @@
 package com.university.event_management.repository;
 
 import com.university.event_management.model.EventApproval;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,5 +13,8 @@ public interface EventApprovalRepository extends JpaRepository<EventApproval, In
     List<EventApproval> findByEventId(Integer eventId);
 
     List<EventApproval> findByStatusAndEventId(String status, Integer eventId);
+
+    @Transactional
+    void deleteByEventId(Integer eventId);
 
 }
